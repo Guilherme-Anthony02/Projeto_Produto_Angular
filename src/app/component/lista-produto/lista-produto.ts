99b0d3c26d548a036b48b294af3706a5c33ae139
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-09-08 20:38:27 -0300

    principais pastas/components do projeto adicionadas

---
+++ src/app/component/lista-produto/lista-produto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-lista-produto',
+  styleUrl: './lista-produto.css',
+  templateUrl: './lista-produto.html',
+})
+export class ListaProduto {}
