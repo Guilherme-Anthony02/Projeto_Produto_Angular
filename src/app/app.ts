fa4d7f4181d4a34b566563916a203d2e98f70430
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-09-09 12:22:53 -0300

    padroniza títulos e textos do projeto para Projeto Produto

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('Projeto_PessoaBD');
+  protected readonly title = signal('Projeto_Produto');
 }
