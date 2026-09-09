ecc6fe52d1666ce0b825492880c9c574ebac74fd
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-09-08 21:45:28 -0300

    modificações realizadas

--- src/app/service/produto-service.ts
+++ src/app/service/produto-service.ts
@@ -1,5 +1,7 @@
-import { Service } from '@angular/core';
-
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Produto } from '../models/Produtos';
 @Service({
     providenIn: 'root'
 })
@@ -16,37 +18,37 @@ export class ProdutoService {//DECLARAÇÃO CONSTRUTOR
   }
 
   //LISTAR ATLETAS NA API
-  listarProduto(): Observable<Pesso[]> {
+  listarProduto(): Observable<Produto[]> {
     //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
     const urlApi = `http://127.0.0.1:8000/produto/`
 
-    return this.http.get<Pessoa[]>(urlApi)
+    return this.http.get<Produto[]>(urlApi)
   }
 
   //LISTAR ATLETA
-  listarAtleta(idAtleta: number):Observable<Pessoa>{
+  listarProduto(idProduto: number):Observable<Produto>{
     //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
 
     const urlApi = `http://127.0.0.1:8000/produto/${idProduto}`
 
-    return this.http.get<Pessoa>(urlApi)
+    return this.http.get<Produto>(urlApi)
   }
 
   //EXCLUIR NA API
-  excluirProduto(atleta: Pessoa): Observable<Pessoa> {
+  excluirProduto(produto: Produto): Observable<Produto> {
     //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
 
     const urlApi = `http://127.0.0.1:8000/produto/${produto.id}`
 
-    return this.http.delete<Pessoa>(urlApi)
+    return this.http.delete<Produto>(urlApi)
   }
 
   //ALTERAR NA API
-  alterarAtleta(atleta: Pessoa):Observable<Pessoa>{
+  alterarProduto(produto: Produto):Observable<Produto>{
     //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
 
-    const urlApi = `http://127.0.0.1:8000/pessoa/${atleta.id}`
+    const urlApi = `http://127.0.0.1:8000/produto/${produto.id}`
 
-    return this.http.put<Pessoa>(urlApi, atleta)
+    return this.http.put<Produto>(urlApi, produto)
   }
 }
