1366d1e727e87e45f03975184953af960488e406
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-09-09 12:22:21 -0300

    ajusta serviço de produtos para listagem, edição e exclusão correta

--- src/app/service/produto-service.ts
+++ src/app/service/produto-service.ts
@@ -1,54 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 import { Produto } from '../models/Produtos';
-@Service({
-    providenIn: 'root'
+
+@Injectable({
+  providedIn: 'root',
 })
-export class ProdutoService {//DECLARAÇÃO CONSTRUTOR
-  constructor(private http: HttpClient) { }
+export class ProdutoService {
+  constructor(private http: HttpClient) {}
 
-  //ADICIONAR NA API
   adicionarProduto(produto: Produto): Observable<Produto> {
-    //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
+    const urlApi = 'http://127.0.0.1:8000/produto/';
 
-    const urlApi = `http://127.0.0.1:8000/produto/`
-
-    return this.http.post<Produto>(urlApi, produto)
+    return this.http.post<Produto>(urlApi, produto);
   }
 
-  //LISTAR ATLETAS NA API
-  listarProduto(): Observable<Produto[]> {
-    //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
-    const urlApi = `http://127.0.0.1:8000/produto/`
+  listarProdutos(): Observable<Produto[]> {
+    const urlApi = 'http://127.0.0.1:8000/produto/';
 
-    return this.http.get<Produto[]>(urlApi)
+    return this.http.get<Produto[]>(urlApi);
   }
 
-  //LISTAR ATLETA
-  listarProduto(idProduto: number):Observable<Produto>{
-    //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
+  listarProduto(idProduto: number): Observable<Produto> {
+    const urlApi = `http://127.0.0.1:8000/produto/${idProduto}`;
 
-    const urlApi = `http://127.0.0.1:8000/produto/${idProduto}`
-
-    return this.http.get<Produto>(urlApi)
+    return this.http.get<Produto>(urlApi);
   }
 
-  //EXCLUIR NA API
   excluirProduto(produto: Produto): Observable<Produto> {
-    //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
+    const urlApi = `http://127.0.0.1:8000/produto/${produto.idproduto}`;
 
-    const urlApi = `http://127.0.0.1:8000/produto/${produto.id}`
-
-    return this.http.delete<Produto>(urlApi)
+    return this.http.delete<Produto>(urlApi);
   }
 
-  //ALTERAR NA API
-  alterarProduto(produto: Produto):Observable<Produto>{
-    //const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
+  exluirProduto(produto: Produto): Observable<Produto> {
+    return this.excluirProduto(produto);
+  }
 
-    const urlApi = `http://127.0.0.1:8000/produto/${produto.id}`
+  alterarProduto(produto: Produto): Observable<Produto> {
+    const urlApi = `http://127.0.0.1:8000/produto/${produto.idproduto}`;
 
-    return this.http.put<Produto>(urlApi, produto)
+    return this.http.put<Produto>(urlApi, produto);
   }
 }
